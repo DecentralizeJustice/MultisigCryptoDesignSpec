a6d2d53eb00c18bfdd384c3521618bb3a152a0de
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-21 19:34:21 -0800

    Revert "Created Site"
    This reverts commit 2fe0f0d31e46881ed2e82119f40e7035f17362f5.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  title: 'MC Documentation',
+  description: 'A Collaborative Endeavour',
+  base: '/docs/',
+  head: [
+    ['link', { rel: 'shortcut icon', type: 'image/x-icon', href: `favicon.ico` }]
+  ],
+  themeConfig: {
+    nav: [
+      { text: 'Intro', link: '/intro/' },
+      { text: 'Github', link: 'https://github.com/DecentralizeJustice/MultisigCryptoDocumentation' },
+      { text: 'Reddit', link: 'https://www.reddit.com/r/DecentralizeJustice' }
+    ],
+    sidebar: [
+      '/intro/',
+      '/prepurchase/',
+      '/purchase/',
+      '/hardware/',
+      '/support/',
+      '/cautions/',
+      '/dict/',
+      '/behindthescene/'
+
+    ]
+  }
+}
