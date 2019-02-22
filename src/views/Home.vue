7a8479e6c4fd4972efb3557ee495dad37fef0e29
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-21 18:33:52 -0800

    Under Construction page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" style="width:25vw;height:auto;">
+    <HelloWorld/>
   </div>
 </template>
 
